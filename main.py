41822b30b9c7a883d535a26b81a13d5e51d64f8b
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2018-09-03 14:18:54 -0400

    csv export

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ soup = BeautifulSoup(page, 'html.parser')
 listOfCards = soup.findAll('tr', attrs={'class': 'content'})
 
 cardMatrix = numpy.matrix(["Card Name", "Lowest Price", "Average Price", "Highest Price"])
+cardNameSet = {"Card Name"}
+
+
+f = open('CyberneticHorizon.csv', 'w')
 
 for card in listOfCards:
 # filter out all ebay links
-    if "ebay" not in card.text:
-        print(card.text)
+    if "eBay" not in card.text:
+        f.write(card.contents[3].text.replace("\n", ""))
+        f.write("\n")
+       # cardNameSet.add(card.contents[3].text.replace("\n", ""))
 
+f.close()
 
-print("hello world")
+print(cardNameSet)
